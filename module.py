15eee5e8df0f53076c99bd9eee51d9911e4acbd9
Ghost-Eyes22 <isaiah.torres@stu.vertexacademies.org>
2026-02-12 19:17:06 +0000

    commit

--- module.py
+++ module.py
@@ -2,3 +2,24 @@ class Robot:
     def __init__(self, name, color):
         # initialize properties code here
         pass
+
+    def charge(self, amount):
+        self.battery = self.battery + charge
+        return f("{self.name}got {self.charge}")
+    
+    def repair(self,amount):
+        self.repair
+    
+    def work(self, amount):
+        self.work += int(amount)
+        return self.work 
+        
+    def status():
+        
+        
+    def recolor():
+        
+    def shutdown():
+        self.shutdown = shutdown -
+        if self.battery = 0:
+            self.shutdown
