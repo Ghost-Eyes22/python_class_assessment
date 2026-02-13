31e9569f0cbb67abde21b6cd6dda7df0ef60c9d0
Ghost-Eyes22 <isaiah.torres@stu.vertexacademies.org>
2026-02-13 19:15:11 +0000

    commit

--- module.py
+++ module.py
@@ -1,25 +1,33 @@
 class Robot:
     def __init__(self, name, color):
-        # initialize properties code here
+# initialize properties code here
         pass
+        self.name = name
+        self.color = color
+        self.battery = 100
+        self.durability = 100
+        self.productivity = 0
 
     def charge(self, amount):
-        self.battery = self.battery + charge
-        return f("{self.name}got {self.charge}")
+        amount = int(amount)
+        self.battery += amount
+        if self.battery < 100:
+            self.battery = self.battery + amount
+        return f"{self.name} charged by {amount} "
     
     def repair(self,amount):
-        self.repair
+        self.repair += int(amount)
+        return self.repair
     
     def work(self, amount):
+        hours = int(hours)
         self.work += int(amount)
         return self.work 
         
-    def status():
-        
+    def status(self):
+        return f"({self.name}| Color: {self.color}| Durability: {self.durability})"
         
     def recolor():
         
-    def shutdown():
-        self.shutdown = shutdown -
-        if self.battery = 0:
-            self.shutdown
+        def shutdown(self, amount):
+            pass
